2a8c8fbb54fddf1dbcc49a32cf982b7d21513a43
Adeola Oladeji <deolaoladeji@gmail.com>
2022-03-01 15:09:58 +0100

    work on services and dtos

--- src/africa/semicolon/sendAm/services/UserServiceImpl.java
+++ src/africa/semicolon/sendAm/services/UserServiceImpl.java
@@ -1,17 +1,28 @@
 package africa.semicolon.sendAm.services;
 
+import africa.semicolon.sendAm.data.models.User;
 import africa.semicolon.sendAm.data.repositories.UserRepository;
+import africa.semicolon.sendAm.data.repositories.UserRepositoryImpl;
 import africa.semicolon.sendAm.dtos.requests.RegisterUserRequests;
 import africa.semicolon.sendAm.dtos.responses.RegisterUserResponse;
 
 public class UserServiceImpl implements UserService {
+    private UserRepository userRepository = new UserRepositoryImpl();
+
     @Override
     public RegisterUserResponse register(RegisterUserRequests requestForm) {
+        User user =new User();
+        user.setEmail(requestForm.getEmailAddress());
+        user.setAddress(requestForm.getAddress());
+        user.setPhoneNumber(requestForm.getPhoneNumber());
+        user.setFullName(requestForm.getFirstName() + " " + requestForm.getLastName());
+        userRepository.save(user);
+
         return null;
     }
 
     @Override
     public UserRepository getRepository() {
-        return null;
+        return userRepository;
     }
 }
--- test/africa/semicolon/sendAm/services/UserServiceImplTest.java
+++ test/africa/semicolon/sendAm/services/UserServiceImplTest.java
@@ -13,19 +13,30 @@ class UserServiceImplTest {
         userService = new UserServiceImpl();
     }
     @Test
-    void after_register_repository_has_one_element() {}{
+    void after_register_repository_has_one_element() {
         //given
-        RegisterUserRequests registerForm = new RegisterUserRequests();
-        registerForm.setFirstName("Lota");
-        registerForm.setLastName("Senior");
-        registerForm.setEmailAddress("Lota@gail.com");
-        registerForm.setAddress("Code Cold Hell");
-        registerForm.setPhoneNumber("2MillionDolars");
+        RegisterUserRequests registerForm = createRegisterForm();
 
         //when
         userService.register(registerForm);
 
         //assert
         assertEquals(1, userService.getRepository().count());
+    }
+
+
+    @Test
+    void duplicate_emails_throws_exception(){
+
+    }
+
+    private RegisterUserRequests createRegisterForm() {
+        RegisterUserRequests registerForm = new RegisterUserRequests();
+        registerForm.setFirstName("Lota");
+        registerForm.setLastName("Senior");
+        registerForm.setEmailAddress("Lota@gail.com");
+        registerForm.setAddress("Code Cold Hell");
+        registerForm.setPhoneNumber("2MillionDolars");
+        return registerForm;
     }
 }
